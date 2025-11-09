52596557c937095a99682593a31829dcc3cf542c
Timur Stupin <hache03@gmail.com>
2025-11-09 20:37:59 +0300

    fix: imports get endpoint duplication

--- src/main/java/ru/itmo/se/is/feature/fileimport/api/ImportController.java
+++ src/main/java/ru/itmo/se/is/feature/fileimport/api/ImportController.java
@@ -1,36 +1,29 @@
 package ru.itmo.se.is.feature.fileimport.api;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import ru.itmo.se.is.feature.fileimport.api.dto.FileUploadRequestDto;
-import ru.itmo.se.is.feature.fileimport.api.dto.ImportOperationLazyBeanParamDto;
 import ru.itmo.se.is.feature.fileimport.api.dto.ImportOperationResponseDto;
-import ru.itmo.se.is.feature.fileimport.application.ImportOperationService;
 import ru.itmo.se.is.feature.fileimport.application.ImportService;
 import ru.itmo.se.is.platform.web.util.MultipartImportFileUtil;
 
 @Path("/imports")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.MULTIPART_FORM_DATA)
 public class ImportController {
 
     @Inject
     private MultipartImportFileUtil multipartImportFileUtil;
     @Inject
     private ImportService importService;
-    @Inject
-    private ImportOperationService importOperationService;
-
-    @GET
-    public Response getAllImportOperations(@BeanParam ImportOperationLazyBeanParamDto lazyBeanParamDto) {
-        return Response.ok(importOperationService.lazyGet(lazyBeanParamDto)).build();
-    }
-
 
     @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response importMovies(MultipartFormDataInput input) {
         FileUploadRequestDto requestDto = multipartImportFileUtil.from(input);
         ImportOperationResponseDto responseDto = importService.importMovies(requestDto);
